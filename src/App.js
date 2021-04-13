08e055a520c07a60e7645f612c3404cedac1e6d4
RiosAlejandro <alexandro.aa86@gmail.com>
2021-04-13 14:18:56 -0300

    avances en el diseño

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
-//import './App.css';
+import Formulario from './Components/Formulario'
+import './App.css';
 
 function App() {
   return (
     <Fragment>
       <h1>Registration Form</h1>
+
+      <Formulario />
     </Fragment>
   );
 }
--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,9 +1,59 @@
 import React, { Fragment } from 'react';
+import './Formulario.css'
 
 const Formulario = () => {
     return ( 
         <Fragment>
-            
+            <form>
+                <div className="tituloFormulario">
+                    <span>Foto centrada y en redondo</span>
+                </div>
+                <div className="camposNombre">
+                    <div className="subCamposNombre">
+                        <input 
+                            placeholder= "First Name"
+                        />
+                    </div>
+                    <div className="subCamposNombre">
+                        <input 
+                            placeholder= "Last Name"
+                        />
+                    </div>
+                </div>
+                <div className="camposFormulario">
+                    <span><i class="fas fa-hand-paper"></i></span>
+                    <input 
+                        placeholder= "Username"
+                    />
+                </div>
+                <div className="camposFormulario">
+                    <span></span>
+                    <input 
+                        placeholder= "Email"
+                    />
+                </div>
+                <div className="camposFormulario">
+                    <span></span>
+                    <input 
+                        placeholder= "Tel. Number"
+                    />
+                </div>
+                <div className="camposFormulario">
+                    <span></span>
+                    <input 
+                        placeholder= "Password"
+                    />
+                </div>
+                <div className="camposFormulario">
+                    <span></span>
+                    <input 
+                        placeholder= "Confirm Password"
+                    />
+                </div>
+                <div>
+                    <button>Ok</button>
+                </div>
+            </form>
         </Fragment>
      );
 }
