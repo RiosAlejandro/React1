1c472cec684082c0f5e0c5aebcbd696e1fb5caa7
RiosAlejandro <alexandro.aa86@gmail.com>
2021-04-16 12:07:42 -0300

    comienzo de validacion de datos

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
+import { useForm } from 'react-hook-form';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const { register, formState: {errors} , handleSubmit } = useForm();
+
+    const onSubmit = (data) => {console.log(data);}
+
     return ( 
         <Fragment>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="fotoFormulario">
                     <input type="file" name="imagen" />
                 </div>
@@ -12,43 +18,63 @@ const Formulario = () => {
                     <div className="subCamposNombre">
                         <input 
                             placeholder= "First Name"
+                            name="firstName"
+                            {...register("firstName", {required: true, minLength: 2, maxLength: 20, pattern: /^[A-Za-z]+$/i} )}
                         />
+                        {errors.firstName && "First name is required"}
                     </div>
                     <div className="subCamposNombre">
                         <input 
                             placeholder= "Last Name"
+                            name="lastName"
+                            {...register("lastName", {required: true, minLength: 2, maxLength: 20, pattern: /^[A-Za-z]+$/i})}
                         />
+                        {errors.lastName && "Last name is required"}
                     </div>
                 </div>
                 <div className="camposFormulario">
                     <span><i class="fas fa-hand-paper"></i></span>
                     <input 
                         placeholder= "Username"
+                        name="username"
+                        {...register("username", {required: true, minLength: 4, maxLength: 12})}
                     />
+                    {errors.username && "Username is required"}
                 </div>
                 <div className="camposFormulario">
                     <span></span>
                     <input 
                         placeholder= "Email"
+                        name="email"
+                        {...register("email", {required: true})}/*poner patron*/ 
                     />
+                    {errors.email && "Email is required"}
                 </div>
                 <div className="camposFormulario">
                     <span></span>
                     <input 
                         placeholder= "Tel. Number"
+                        name="telNumber"
+                        {...register("telNumber", {required: false})}
                     />
                 </div>
                 <div className="camposFormulario">
                     <span></span>
                     <input 
                         placeholder= "Password"
+                        name="password"
+                        {...register("password", {required: true, minLength: 6, maxLength: 30})}
                     />
+                    {errors.password && "password is required"}
                 </div>
                 <div className="camposFormulario">
                     <span></span>
                     <input 
                         placeholder= "Confirm Password"
+                        name="confirmPassword"
+                        
                     />
+                    {errors.confirmPassword && "passwords do not match"}
                 </div>
                 <div className="botonFormulario">
                     <button>Ok</button>
